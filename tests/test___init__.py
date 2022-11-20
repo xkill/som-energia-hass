f5be82b809aff5c7011753455eb64a4704fb3dc8
xkill <pablo.catalina@gmail.com>
2022-11-20 22:23:57 +0100

    Actualizar precios en test

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -5,15 +5,15 @@ from custom_components.som_energia.price import price
 
 def test_price_on_monday():
     monday = datetime(2022, 1, 24, 0, 0, 0)
-    assert price(monday) == 0.284556
+    assert price(monday) == 0.234
 
     monday = datetime(2022, 1, 24, 8, 0, 0)
-    assert price(monday) == 0.34171
+    assert price(monday) == 0.281
 
     monday = datetime(2022, 1, 24, 10, 0, 0)
-    assert price(monday) == 0.417105
+    assert price(monday) == 0.343
 
 
 def test_price_on_sunday():
     sunday = datetime(2022, 1, 23, 16, 57, 0)
-    assert price(sunday) == 0.284556
+    assert price(sunday) == 0.234
