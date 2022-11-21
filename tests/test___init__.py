e08ca3a961ae8734616d98d7c2e5ff45ca328fb8
Pablo Catalina <xkill@locolandia.net>
2022-11-21 10:27:47 +0100

    Updated prices and added compensation

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -1,19 +1,22 @@
 from datetime import datetime
 
-from custom_components.som_energia.price import price
+from custom_components.som_energia.price import price, compensation
 
 
 def test_price_on_monday():
     monday = datetime(2022, 1, 24, 0, 0, 0)
-    assert price(monday) == 0.284556
+    assert price(monday) == 0.234
 
     monday = datetime(2022, 1, 24, 8, 0, 0)
-    assert price(monday) == 0.34171
+    assert price(monday) == 0.281
 
     monday = datetime(2022, 1, 24, 10, 0, 0)
-    assert price(monday) == 0.417105
+    assert price(monday) == 0.343
 
 
 def test_price_on_sunday():
     sunday = datetime(2022, 1, 23, 16, 57, 0)
-    assert price(sunday) == 0.284556
+    assert price(sunday) == 0.234
+
+def test_compesation():
+    assert compensation() == 0.176
